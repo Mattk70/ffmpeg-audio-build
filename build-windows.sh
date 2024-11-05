f4cfa28dc8b95d5775f306750d3632a9e1f4c927
Mattk70 <email@mattkirkland.co.uk>
2024-11-05 12:35:49 +0000

    check /usr/lib and include paths

--- build-windows.sh
+++ build-windows.sh
@@ -24,8 +24,8 @@ tar --strip-components=1 -xf $BASE_DIR/$FFMPEG_TARBALL
 
 FFMPEG_CONFIGURE_FLAGS+=(
     --prefix=$BASE_DIR/$OUTPUT_DIR
-    --extra-cflags='-static -static-libgcc -static-libstdc++ -I/usr/local/include'
-    --extra-ldflags="-L/usr/local/lib"
+    --extra-cflags='-static -static-libgcc -static-libstdc++ -I/usr/include'
+    --extra-ldflags="-L/usr/lib"
     --target-os=mingw32
     --arch=$ARCH
     --cross-prefix=$ARCH-w64-mingw32-
