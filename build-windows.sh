63e73dd14e41d34d711b79cbdf20ac1572a52f00
Mattk70 <email@mattkirkland.co.uk>
2024-11-08 13:20:41 +0000

    echo

--- build-windows.sh
+++ build-windows.sh
@@ -25,14 +25,14 @@ tar --strip-components=1 -xf $BASE_DIR/$FFMPEG_TARBALL
 EXTRA_CONFIGURE_FLAGS=(
     --prefix=$BASE_DIR/$OUTPUT_DIR
     "--extra-cflags='-static -static-libgcc -static-libstdc++'"
-    #--extra-ldflags='-L/usr/local/x86_64-w64-mingw32/lib'
+    --extra-ldflags='-L/usr/local/x86_64-w64-mingw32/lib'
     --target-os=mingw32
     --arch=$ARCH
     --cross-prefix=$ARCH-w64-mingw32-
 )
-echo $EXTRA_CONFIGURE_FLAGS
+echo "${EXTRA_CONFIGURE_FLAGS[@]} ${FFMPEG_CONFIGURE_FLAGS[@]}"
 
-./configure $EXTRA_CONFIGURE_FLAGS ${FFMPEG_CONFIGURE_FLAGS[@]}
+./configure "${EXTRA_CONFIGURE_FLAGS[@]} ${FFMPEG_CONFIGURE_FLAGS[@]}"
 make
 make install
 chown $(stat -c '%u:%g' $BASE_DIR) -R $BASE_DIR/$OUTPUT_DIR
