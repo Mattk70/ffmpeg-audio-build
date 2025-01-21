77737e9febf6276dfc55f0e8e9cea2a0a839301b
mattk70 <email@mattkirkland.co.uk>
2025-01-21 09:59:04 +0000

    setup build-windows for mingw

--- build-windows.sh
+++ build-windows.sh
@@ -22,11 +22,17 @@ trap 'rm -rf $BUILD_DIR' EXIT
 cd $BUILD_DIR
 tar --strip-components=1 -xf $BASE_DIR/$FFMPEG_TARBALL
 
+# Set cross-compilation flags for MinGW
+export CC=x86_64-w64-mingw32-gcc
+export CXX=x86_64-w64-mingw32-g++
+export AR=x86_64-w64-mingw32-ar
+export RANLIB=x86_64-w64-mingw32-ranlib
+
 FFMPEG_CONFIGURE_FLAGS+=(
-    --cc=gcc
+    # --cc=gcc
     --prefix=$BASE_DIR/$OUTPUT_DIR
-    --extra-cflags='-static -static-libgcc -static-libstdc++ -I/usr/local/include'
-    --extra-ldflags='-L/usr/local/lib'
+    --extra-cflags='-static -static-libgcc -static-libstdc++ -I/mingw64/include'
+    --extra-ldflags='-L/mingw64/lib'
     --target-os=mingw32
     --arch=$ARCH
     --cross-prefix=$ARCH-w64-mingw32-
